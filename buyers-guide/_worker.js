182110300f2ec784080c1f81c4a1bcc95f101ee7
Daniel <dedaff@yahoo.com>
2026-05-14 19:00:11 -0700

    Add buyers-guide-route support

--- buyers-guide/_worker.js
+++ buyers-guide/_worker.js
@@ -1,12 +1,3 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-
-    const stripped =
-      url.pathname.replace(/^\/buyers-guide/, "") || "/";
-
-    return env.ASSETS.fetch(
-      new Request(url.origin + stripped + url.search, request)
-    );
-  }
-};
+const stripped = url.pathname
+  .replace(/^\/third-party-maintenance/, '')
+  .replace(/^\/buyers-guide/, '') || '/';
