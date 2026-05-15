bc2a72c8e90c36111531e52126da1d4fb322b79b
Daniel <dedaff@yahoo.com>
2026-05-14 19:16:30 -0700

    deleted worker.js in buyers-guide

--- buyers-guide/_worker.js
+++
@@ -1,9 +0,0 @@
-let stripped = url.pathname;
-
-if (url.pathname.startsWith('/third-party-maintenance')) {
-  stripped = url.pathname.replace(/^\/third-party-maintenance/, '') || '/';
-}
-
-if (url.pathname.startsWith('/buyers-guide')) {
-  stripped = '/buyers-guide/index.html';
-}
