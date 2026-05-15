d63ac46b039cac94d2b0fa7ad4e92a4e2e089ea1
Daniel <dedaff@yahoo.com>
2026-05-14 19:06:44 -0700

    fix buyers guide worker routing

--- buyers-guide/_worker.js
+++ buyers-guide/_worker.js
@@ -1,3 +1,9 @@
-const stripped = url.pathname
-  .replace(/^\/third-party-maintenance/, '')
-  .replace(/^\/buyers-guide/, '') || '/';
+let stripped = url.pathname;
+
+if (url.pathname.startsWith('/third-party-maintenance')) {
+  stripped = url.pathname.replace(/^\/third-party-maintenance/, '') || '/';
+}
+
+if (url.pathname.startsWith('/buyers-guide')) {
+  stripped = '/buyers-guide/index.html';
+}
